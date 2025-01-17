3ba68a7812bd5a65c9f51d05d2e80753594eb43e
kienvc <17985897+kienvc@users.noreply.github.com>
2025-01-17 19:09:13 +0700

    change base url to env

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { type UUID, type Character } from "@elizaos/core";
 
-const BASE_URL = `http://localhost:${import.meta.env.VITE_SERVER_PORT}`;
+const BASE_URL = `${import.meta.env.AGENT_BASE_URL}`;
 
 const fetcher = async ({
     url,
